9dc94dd05edf80b7f8259ec642b5afd04070441e
Echo Wang <ywang623@my.bcit.ca>
2025-02-07 09:35:20 -0800

    Add health check endpoint

--- labs/4/server.js
+++ labs/4/server.js
@@ -19,6 +19,10 @@ class Server {
   handleRequest(req, res) {
     const parsedUrl = url.parse(req.url, true);
     let query = parsedUrl.query;
+    if (req.method === "GET" && req.url === "/health") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      return res.end("OK");
+    }
     if (parsedUrl.pathname === "/api/definitions") {
       if (req.method === "GET") {
         this.searchHandler(query, res);
@@ -81,7 +85,10 @@ class Server {
     } else {
       const newItem = new Definition(word, definition);
       this.dictionary.push(newItem);
-      this.handleSuccess(res, newItem);
+      this.handleSuccess(res, {
+        ...newItem,
+        wordCount: this.dictionary.length,
+      });
     }
   }
 
