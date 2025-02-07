526990ce3e8afb7acb5268e85c03fb90451ea08b
Echo Wang <ywang623@my.bcit.ca>
2025-02-06 19:41:21 -0800

    Add COMP4537 Lab4

---
+++ labs/4/lang/en/en.js
@@ -0,0 +1,7 @@
+module.exports = {
+  wordNotFound: "The word %1 was not found in our dictionary",
+  wordAlreadyExists: "The word %1 already exists in our dictionary",
+  wordAdded: "The word %1 was added to our dictionary",
+  badRequest: "Word or definition is missing",
+  notFound: "404 Not Found",
+};
